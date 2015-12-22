cf1bf502b97376538b50017f52db11900b824e93
Alexander <achat@uom.gr>
2015-12-22 12:32:30 +0200

    testMethod2

--- src/Course.java
+++ src/Course.java
@@ -12,5 +12,13 @@ public class Course {
 		capacity = 60;
 		System.out.println(name + location + capacity);
 	}
+	
+public void testMethod2() {
+		
+		name = "Java";
+		location = "KEYP";
+		capacity = 60;
+		System.out.println(name + location + capacity);
+	}
 
 }
