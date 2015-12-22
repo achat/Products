fdcaed3ccadfaeced37ae7564a5884e73ba97d28
Alexander <achat@uom.gr>
2015-12-22 12:30:43 +0200

    First commit

---
+++ src/Course.java
@@ -0,0 +1,16 @@
+
+public class Course {
+	
+	private String name;
+	private String location;
+	private int capacity;
+	
+	public void testMethod() {
+		
+		name = "Java";
+		location = "KEYP";
+		capacity = 60;
+		System.out.println(name + location + capacity);
+	}
+
+}
